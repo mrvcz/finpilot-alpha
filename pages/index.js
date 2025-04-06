2b75d41f037f4218cd5e8b0374d4d70f841d18e2
Dominik Pozniak <domiprzemhq@gmail.com>
2025-04-06 22:43:17 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function DevnetDashboard() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-3xl font-bold mb-2">🚧 Finpilot Alpha</h1>
+      <p className="text-gray-400">This is a devnet/testnet version of the Finpilot dashboard.</p>
+    </div>
+  );
+}
